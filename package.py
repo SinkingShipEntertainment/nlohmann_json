566024c5e4acaa03e033fdbadeb0ff418d966f33
Marcelo Sercheli <msercheli@gmail.com>
2023-05-04 09:47:11 -0400

    Add rez files

---
+++ package.py
@@ -0,0 +1,43 @@
+name = 'nlohmann_json'
+
+version = '3.7.3'
+
+authors = [
+    'Niels Lohmann',
+]
+
+description = '''Json for Modern C++'''
+
+with scope('config') as c:
+    # Determine location to release: internal (int) vs external (ext)
+    # NOTE: Modify this variable to reflect the current package situation
+    release_as = 'ext'
+
+    # The 'c' variable here is actually rezconfig.py
+    # 'release_packages_path' is a variable defined inside rezconfig.py
+
+    import os
+    if release_as == 'int':
+        c.release_packages_path = os.environ['SSE_REZ_REPO_RELEASE_INT']
+    elif release_as == 'ext':
+        c.release_packages_path = os.environ['SSE_REZ_REPO_RELEASE_EXT']
+
+requires = [
+]
+
+private_build_requires = [
+]
+
+variants = [
+    ['platform-linux', 'arch-x86_64', 'os-centos-7'],
+]
+
+def pre_build_commands():
+    command("source /opt/rh/devtoolset-6/enable")
+
+def commands():
+    env.REZ_NLOHMANN_JSON_ROOT = '{root}'
+
+
+build_system = "cmake"
+uuid = 'repository.nlohmann_json'
